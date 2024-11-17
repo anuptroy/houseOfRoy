89cbaf63d425dbed81e6bb7261312225dd5eb382
anuptroy <anup.t.roy@gmail.com>
2024-11-17 21:54:33 +0300

    rearrnage and reorder

--- familyTreeEdit.js
+++ familyTreeEdit.js
@@ -1,13 +1,10 @@
 function getOptions() {
     const searchParams = new URLSearchParams(window.location.search);
     let fit = searchParams.get('fit');
-    let enableSearch = true;
-    let scaleInitial = 1;
-    if (fit === 'yes') {
-        enableSearch = false;
-        scaleInitial = FamilyTree.match.boundary;
-    }
-    return { enableSearch, scaleInitial };
+    return {
+        enableSearch: fit !== 'yes',
+        scaleInitial: fit === 'yes' ? FamilyTree.match.boundary : 1,
+    };
 }
 
 var options = getOptions();
@@ -24,80 +21,48 @@ var chart = new FamilyTree(document.getElementById("tree"), {
     }
 });
 
-//pid: partner id
-//mid: mother id
-//fid: fatherid
-
-
 chart.load([
+    // Ancestors
+    { id: 1, name: "Vetka Roy", title: "", img: "", gender: 'male' }, // Father of Fulchand Roy
+    { id: 2, pids: [1], name: "Mrs Roy", title: "", img: "", gender: 'female' }, // Wife of Vetka Roy
+    { id: 3, fid: 1, mid: 2, pids: [4], name: "Fulchand Roy", title: "", img: "", gender: 'male' },
+    { id: 4, pids: [3], name: "Mrs. SinghaRoy", title: "", img: "", gender: 'female' },
 
-    // Adding Fulchand Roy
-    { id: 31, name: "Vetka Roy", title: "", img: "", gender: 'male' }, // Father of Fulchand Roy
+    // Kelsha's family
+    { id: 5, fid: 3, mid: 4, pids: [6], name: "Kelsha SinghaRoy", img: "", gender: 'male' },
+    { id: 6, pids: [5], name: "Mrs SinghaRoy", title: "", img: "", gender: 'female' },
 
-    // Updating Fulchand Roy to include his father and wife
-    { id: 30, fid: 31, pids: [32], name: "Fulchand Roy", title: "", img: "", gender: 'male' },
+    // Parshuram's family
+    { id: 7, fid: 5, mid: 6, pids: [8], name: "Mrs SinghaRoy", img: "", gender: 'female' },
+    { id: 8, pids: [7], name: "Parshuram SinghaRoy", title: "", img: "", gender: 'male' },
 
-    // Adding Mrs. Fulchandani Roy as Fulchand's wife
-    { id: 32, pids: [30], name: "Mrs. SinghaRoy", title: "", img: "", gender: 'female' },
+    // Rajkishore's family
+    { id: 9, fid: 7, mid: 8, pids: [10], name: "Rajkishore SinghaRoy", title: "", img: "", gender: 'male' },
+    { id: 10, pids: [9], name: "Mrs SinghaRoy1", title: "", img: "", gender: 'female' },
 
-    // Updating Kelsha SinghaRoy to keep the connection to his father
-    { id: 1, fid: 30, pids: [2], name: "Kelsha SinghaRoy", img: "", gender: 'male' },
+    // Sashibhushan's family
+    { id: 11, fid: 9, mid: 10, pids: [12], name: "Sashibhushan Roy", title: "", img: "", gender: 'male' },
+    { id: 12, pids: [11], name: "Shobha Roy", title: "", img: "", gender: 'female' },
 
-    { id: 2, pids: [1], name: "Mrs SinghaRoy", title: "", img: "", gender: 'female' }, //
+    // Tapankumar's generation
+    { id: 13, fid: 11, mid: 12, pids: [14], name: "Tapankumar Roy", img: "images/papa.jpg", gender: 'male' }, // Papa
+    { id: 14, pids: [13], name: "Aparna Roy", title: "", img: "images/mom.jpg", gender: 'female' }, // Wife
+    { id: 15, fid: 11, mid: 12, name: "Jayashree Roy", img: "", gender: 'female' },
+    { id: 16, fid: 11, mid: 12, pids: [17], name: "Swapan Roy", img: "", gender: 'male' }, // Swapan Roy
+    { id: 17, pids: [16], name: "Bichitra Roy", title: "", img: "", gender: 'female' }, // Swapan's wife
+    { id: 18, fid: 11, mid: 12, pids: [19], name: "Jivan Roy", img: "", gender: 'male' }, // Jivan Roy
+    { id: 19, pids: [18], name: "Multi Roy", title: "", img: "", gender: 'female' }, // Jivan's wife
+    { id: 20, fid: 11, mid: 12, name: "Chanchal Roy", title: "Died: 2003", img: "", gender: 'male' }, // Brother of Tapankumar
 
-    { id: 3, pids: [4], mid: 2, fid: 1, name: "Mrs SinghaRoy", img: "", gender: 'female' },
-
-    { id: 4, pids: [3], name: "Parshuram SinghaRoy", title: "", img: "", gender: 'male' },
-
-    { id: 6, mid: 3, fid: 4, pids: [7], name: "Rajkishore SinghaRoy", title: "", img: "", gender: 'male' },
-    { id: 7, pids: [6], name: "Mrs SinghaRoy1", title: "", img: "", gender: 'female' },
-
-    { id: 12, fid: 6, mid: 7, pids: [14], name: "Sashibhushan Roy", title: "", img: "", gender: 'male' }, //Grandpa
-    { id: 14, pids: [12], name: "Shobha Roy", title: "", img: "", gender: 'female' }, //GrandMa
-
-    { id: 16, fid: 12, mid: 14, pids: [20], name: "Tapankumar Roy", img: "images/papa.jpg", gender: 'male' }, //Papa
-    { id: 17, fid: 12, mid: 14, name: "Jayashree Roy", img: "", gender: 'female' },
-    { id: 18, fid: 12, mid: 14, pids: [25], name: "Swapan Roy", img: "", gender: 'male' }, // Swapan Roy
-    { id: 19, fid: 12, mid: 14, pids: [26], name: "Jivan Roy", img: "", gender: 'male' }, // Jivan Roy
-    { id: 29, fid: 12, mid: 14, name: "Chanchal Roy", title: "Died: 2003", img: "", gender: 'male' }, // Brother of Tapankumar Roy
-
-    { id: 20, pids: [16], name: "Aparna Roy", title: "", img: "images/mom.jpg", gender: 'female' }, // Wife of Tapankumar Roy
-
-    // Children of Tapankumar Roy and Aparna Roy
-    { id: 21, fid: 16, mid: 20, pids: [22], name: "Anup Roy", title: "", img: "images/anup.jpg", gender: 'male' }, // Son
+    // Tapankumar's children
+    { id: 21, fid: 13, mid: 14, pids: [22], name: "Anup Roy", title: "", img: "images/anup.jpg", gender: 'male' }, // Son
     { id: 22, pids: [21], name: "Neha Roy", title: "", img: "images/neha.jpg", gender: 'female' }, // Wife of Anup Roy
-    { id: 23, fid: 16, mid: 20, name: "Taniya Roy", title: "", img: "images/nanu.jpg", gender: 'female' }, // Daughter
+    { id: 23, fid: 21, mid: 22, name: "Vibhum Roy", title: "", img: "images/vibhum.jpeg", gender: 'male' }, // Son of Anup and Neha
+    { id: 24, fid: 13, mid: 14, name: "Taniya Roy", title: "", img: "images/nanu.jpg", gender: 'female' }, // Daughter
 
-    // Child of Anup Roy and Neha Roy
-    { id: 24, fid: 21, mid: 22, name: "Vibhum Roy", title: "", img: "images/vibhum.jpeg", gender: 'male' }, // Son of Anup and Neha
+    // Swapan's child
+    { id: 25, fid: 16, mid: 17, name: "Monalisa Roy", title: "", img: "", gender: 'female' }, // Daughter of Swapan and Bichitra
 
-    // Wife of Swapan Roy
-    { id: 25, pids: [18], name: "Bichitra Roy", title: "", img: "", gender: 'female' },
-
-    // Child of Swapan Roy and Bichitra Roy
-    { id: 28, fid: 18, mid: 25, name: "Monalisa Roy", title: "", img: "", gender: 'female' }, // Daughter of Swapan and Bichitra
-
-    // Wife of Jivan Roy
-    { id: 26, pids: [19], name: "Multi Roy", title: "", img: "", gender: 'female' },
-
-    // Child of Jivan Roy and Multi Roy
-    { id: 27, fid: 19, mid: 26, name: "Joydeep Roy", title: "", img: "images/bro.jpg", gender: 'male' } // Son of Jivan and Multi Roy
+    // Jivan's child
+    { id: 26, fid: 18, mid: 19, name: "Joydeep Roy", title: "", img: "images/bro.jpg", gender: 'male' } // Son of Jivan and Multi Roy
 ]);
-
-
-
-
-
-
-function getOptions() {
-    const searchParams = new URLSearchParams(window.location.search);
-    var fit = searchParams.get('fit');
-    var enableSearch = true;
-    var scaleInitial = 1;
-    if (fit == 'yes') {
-        enableSearch = false;
-        scaleInitial = FamilyTree.match.boundary;
-    }
-    return { enableSearch, scaleInitial };
-}
-
